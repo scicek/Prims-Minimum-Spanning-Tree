8a549c6a5d37e02cd730fc33272f974ceea0f99d
scicek <scicek@kth.se>
2013-09-27 00:58:42 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -22,32 +22,6 @@ public class Main
         g.addEdge("Uppsala", "Göteborg", 452);
         g.addEdge("Uppsala", "Malmö", 679);
         g.addEdge("Västerås", "Malmö", 599);
-        /*g.addVertex("A");
-        g.addVertex("B");
-        g.addVertex("C");
-        g.addVertex("D");
-        g.addVertex("E");
-        g.addVertex("F");
-        
-        g.addEdge("A", "B", 10);
-        g.addEdge("A", "C", 20);
-        g.addEdge("A", "E", 25);
-        g.addEdge("A", "F", 50);
-        g.addEdge("B", "C", 11);
-        g.addEdge("C", "D", 12);
-        g.addEdge("D", "E", 13);
-        g.addEdge("D", "B", 30);
-        g.addEdge("E", "F", 14);
-        
-        
-        g.addEdge("A", "C", 20);
-        g.addEdge("A", "B", 30);
-        g.addEdge("A", "D", 40);
-        g.addEdge("B", "D", 20);
-        g.addEdge("B", "E", 50);
-        g.addEdge("C", "D", 10);
-        g.addEdge("C", "E", 90);
-        */
         
         System.out.println("\nResult: " + g.minimumSpandingTree(true));
     }
